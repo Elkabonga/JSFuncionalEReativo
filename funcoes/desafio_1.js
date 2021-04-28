ef70afa7c1be73b8183fd509b5259e71d3038d8c
Tiago Santos <tisgo.slip@hotmail.com>
2021-04-27 22:35:25 -0300

    wip

--- funcoes/desafio_1.js
+++ funcoes/desafio_1.js
@@ -1,7 +1,7 @@
 // somar(3)(4)(5)
-function somar(a) {
-    return function(b) {
-        return function(c) {
+function somar(a = 0) {
+    return function (b = 0) {
+        return function (c = 0) {
             return a + b + c
         }
     }
@@ -11,15 +11,13 @@ const somarAB = somar(3)(4)
 console.log(somarAB(13))
 console.log(somar(13)(20)(30))
 
-
-
 // fn -> 3 * 7
 // fn -> 3 + 7
 // fn -> 3 - 7
 // calcular(3)(7)(fn)
 function calcular(x) {
-    return function(y) {
-        return function(fn) {
+    return function (y) {
+        return function (fn) {
             return fn(x, y)
         }
     }
